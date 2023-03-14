911af8d9aacbf7dc7b4e5d7e8a3bc76ae14fab80
Aedan-Vongsouthi-Student <112557510+Aedan-Vongsouthi-Student@users.noreply.github.com>
2023-03-14 09:57:40 -0400

    Finished Lesson 1

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Sussus Amogus");
+        }
+    }
+}
