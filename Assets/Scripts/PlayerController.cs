99370d9dd55b5d68a4495e76493bcb3e93731634
AedanVongsouthi <aevo910@student.oaisd.org>
2023-03-15 12:42:42 -0400

    Finished lesson

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -22,6 +33,18 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else 
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
